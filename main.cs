caf07ae8ba5eb2c76af06cf97fbf85bcf8bb2888
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-13 17:17:21 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,26 @@
+// Created by: Venika Sem
+// Created on: May 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int userInteger;
+        Console.WriteLine("Input integer (positive or negative):");
+        userInteger = int.Parse(Console.ReadLine());
+        if (userInteger > -1)
+        {
+            userInteger = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Integer is positive.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Integer is negative.");
+        }
+    }
 }
